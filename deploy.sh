997a446fded6958c4cf3057d32514cf7f0754919
ColdEvul <andreas.brostrom.ce@gmail.com>
2019-05-08 11:56:43 +0200

    Added deploy script for automatic building

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+TRAVIS_TAG='1.2.3'
+
+set -e
+
+TAG=$(curl --silent "https://api.github.com/repos/7Cav/cScripts/releases/latest" | \
+    grep '"tag_name":' | \
+    sed -E 's/.*"([^"]+)".*/\1/')
+curl -sOL "https://github.com/7Cav/cScripts/releases/download/$TAG/cScripts-$TAG.zip"
+
+python3 build.py -b sandbox -p cScripts-$TAG.zip -vu ${TRAVIS_TAG} -y
+python3 build.py -b training -p cScripts-$TAG.zip -vu ${TRAVIS_TAG} -y
