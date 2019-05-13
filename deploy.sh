dc794cdb97a4b2c9fd2606851954678554d30ac6
Broström.A | Evul <andreas.brostrom.ce@gmail.com>
2019-05-13 16:51:47 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,16 @@
 
 set -e
 
-echo 'Checking for latest version of cScripts...'
+echo "Checking for latest version of cScripts..." 
 TAG=$(curl --silent "https://api.github.com/repos/7Cav/cScripts/releases/latest" | \
     grep '"tag_name":' | \
     sed -E 's/.*"([^"]+)".*/\1/')
 curl -sOL "https://github.com/7Cav/cScripts/releases/download/$TAG/cScripts-$TAG.zip"
+
+if [ -z "$TAG" ]; then
+    echo "\$TAG is empty this will cause error soon..."
+fi
+
 echo 'Downloaded v$TAG of cScripts (cScripts-$TAG.zip)'
 
 python3 build.py -b sandbox -p cScripts-$TAG.zip -vu ${TRAVIS_TAG} -y
