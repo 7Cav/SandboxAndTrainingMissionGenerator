8f4c49b36a772835ae740c89cf4fcf0255a8397e
Broström.A | Evul <andreas.brostrom.ce@gmail.com>
2019-05-13 16:20:41 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,12 @@
 
 set -e
 
+echo 'Checking for latest version of cScripts...'
 TAG=$(curl --silent "https://api.github.com/repos/7Cav/cScripts/releases/latest" | \
     grep '"tag_name":' | \
     sed -E 's/.*"([^"]+)".*/\1/')
 curl -sOL "https://github.com/7Cav/cScripts/releases/download/$TAG/cScripts-$TAG.zip"
+echo 'Downloaded v$TAG of cScripts (cScripts-$TAG.zip)'
 
 python3 build.py -b sandbox -p cScripts-$TAG.zip -vu ${TRAVIS_TAG} -y
 python3 build.py -b training -p cScripts-$TAG.zip -vu ${TRAVIS_TAG} -y
