670ea5fe38bbf10d100818e7128766bf1fb648a8
Broström.A | Evul <andreas.brostrom.ce@gmail.com>
2019-05-14 10:48:14 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "Checking and downloading latest version of cScripts..." 
-TAG=$(curl --silent "https://api.github.com/repos/7Cav/cScripts/releases/latest" | \
+TAG=$(curl -u "${GH_API_USER}" -f -s "https://api.github.com/repos/7Cav/cScripts/releases/latest" | \
     grep '"tag_name":' | \
     sed -E 's/.*"([^"]+)".*/\1/')
 curl -sOL "https://github.com/7Cav/cScripts/releases/download/$TAG/cScripts-$TAG.zip"
