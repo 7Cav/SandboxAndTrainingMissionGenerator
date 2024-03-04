5da464741ad727ebf90159d1ed0e639ed1ad4d55
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2024-03-04 18:56:29 +0100

    updated

--- deploy.sh
+++ deploy.sh
@@ -28,3 +28,8 @@ python3 build.py sandbox -p cScripts-$PACKAGE_TAG.zip -pv $PACKAGE_TAG -v ${VERS
 python3 build.py sandbox -s setup_NoRadios.json -o noradio -p cScripts-$PACKAGE_TAG.zip -pv $PACKAGE_TAG -v ${VERSION_TAG} -y
 
 #python3 build.py training -p cScripts-$PACKAGE_TAG.zip -pv $PACKAGE_TAG -v ${VERSION_TAG} -y
+
+mkdir -p server_missions
+#unzip release/Mission_sandbox_v$VERSION_TAG.zip -d server_missions
+#unzip release/Mission_sandbox_noradio_v$VERSION_TAG.zip-d server_missions
+touch "server_missions/fakemissionfile$VERSION_TAG.txt"
